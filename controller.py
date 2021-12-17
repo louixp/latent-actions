c49d940c0cf4c74c20150bc51965d7bc92132227
Louis Qin <qyl0509@icloud.com>
2021-12-17 00:45:28 -0800

    Throws an exception when back button is pressed to signal exit.

--- controller.py
+++ controller.py
@@ -3,6 +3,11 @@ from typing import Tuple
 import pygame
 import torch
 
+
+class ExitException(Exception):
+    pass
+
+
 class Controller:
 
     def __init__(self, scale: int = 1, DoF: int = 2):
@@ -17,9 +22,30 @@ class Controller:
 
     def get_action(self) -> torch.Tensor:
         pygame.event.get()
+        
+        if self.joystick.get_button(8):
+            raise ExitException('Back button pushed. Exiting Simulation.')
+
         axes = [self.joystick.get_axis(0), 
                 self.joystick.get_axis(1), 
                 self.joystick.get_axis(2), 
                 self.joystick.get_axis(1)]
         action = torch.tensor([axes[:self.DoF]])
         return self.scale * action
+
+
+if __name__ == '__main__':
+    joystick = Controller().joystick
+    print('Buttons\t', end='\t')
+    print('Axes')
+
+    while True:
+        _ = pygame.event.get()
+        for i in range(joystick.get_numbuttons()):
+            print(joystick.get_button(i), end='')
+        print(end='\t')
+
+        for i in range(joystick.get_numaxes()):
+            print(f'{joystick.get_axis(i):2f}', end='\t')
+
+        print(end='\r') 
