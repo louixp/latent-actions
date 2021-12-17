93b384ac78649f70b5766883220052cbf1a14ebc
Louis Qin <qyl0509@icloud.com>
2021-12-17 02:32:29 -0800

    Split out visualization from example.py

--- example.py
+++ example.py
@@ -1,13 +1,9 @@
 from argparse import ArgumentParser
 import multiprocessing as mp
 import platform
-import sys
 from time import sleep 
 from typing import Iterable
 
-from mpl_toolkits.mplot3d import axes3d
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 import numpy as np
 import torch
 import gym
@@ -15,90 +11,10 @@ import panda_gym
 
 from cvae import cvae 
 from controller import Controller
+import visualization
+
 
 ACTION_SCALE = 10
-
-def visualize_manifold(
-        decoder: cvae.VAE, 
-        conn: mp.connection.Connection):
-    x, y, z = np.meshgrid(np.arange(-ACTION_SCALE, ACTION_SCALE, 1),
-                          np.arange(-ACTION_SCALE, ACTION_SCALE, 1),
-                          0)
-    latent_actions = np.concatenate((x, y), axis=-1)
-    latent_actions = torch.from_numpy(latent_actions.reshape((-1, 2))).float() 
-    latent_actions *= ACTION_SCALE
-    norm = np.linalg.norm(latent_actions, axis=-1)
-
-    def plot_function(i):
-        ax.cla() 
-        
-        try:
-            prev_action, context = conn.recv()        
-        except:
-            print('Manifold visualization exiting')
-            sys.exit()
-        prev_action = prev_action.numpy()[0]
-        contexts = context.expand(latent_actions.shape[0], context.shape[1])
-
-        decoded_actions = decoder(latent=latent_actions, context=contexts)
-        decoded_actions = decoded_actions.detach().numpy()[:, :3]
-        decoded_actions = decoded_actions.reshape((x.shape[0], x.shape[1], 3))
-
-        u, v, w = np.split(decoded_actions, 3, axis=-1) 
-        sc = ax.scatter(u, v, w, c=norm)
-
-    fig = plt.figure(figsize=plt.figaspect(0.5))
-    ax = fig.add_subplot(1, 2, 1, projection='3d')
-    ax_ref = fig.add_subplot(1, 2, 2)
-    ax.set_title('Decodable Action Manifold')
-    ax_ref.set_title('Reference Latent Manifold')
-    ax_ref.scatter(x, y, c=norm)
-    ani = FuncAnimation(fig, plot_function, interval=1)
-    plt.show()
-
-
-def visualize_vector_field(
-        decoder: cvae.VAE, 
-        conn: mp.connection.Connection):
-    x, y, z = np.meshgrid(np.arange(-ACTION_SCALE, ACTION_SCALE, 2),
-                          np.arange(-ACTION_SCALE, ACTION_SCALE, 2),
-                          0)
-    latent_actions = np.concatenate((x, y), axis=-1)
-    latent_actions = torch.from_numpy(latent_actions.reshape((-1, 2))).float() 
-    latent_actions *= ACTION_SCALE
-
-    def plot_function(i):
-        ax.cla() 
-        
-        try:
-            prev_action, context = conn.recv()        
-        except Exception as e:
-            print('Vector field visualization exiting')
-            sys.exit()
-        prev_action = prev_action.numpy()[0]
-        contexts = context.expand(latent_actions.shape[0], context.shape[1])
-
-        decoded_actions = decoder(latent=latent_actions, context=contexts)
-        decoded_actions = decoded_actions.detach().numpy()[:, :3]
-        decoded_actions = decoded_actions.reshape((x.shape[0], x.shape[1], 3))
-
-        u, v, w = np.split(decoded_actions, 3, axis=-1) 
-        ax.quiver(x, y, z, u, v, w, normalize=True)
-
-        # Controller/latent current posistion.
-        ax.plot(*prev_action, 'ro')
-        # Small hacks to 'equalize' axes since matplotlib doesn't support it.
-        ax.plot(0, 0, 2.5, alpha=0)
-        ax.plot(0, 0, -2.5, alpha=0)
-
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    ax.set_xlabel('Latent x')
-    ax.set_ylabel('Latent y')
-    ax.set_title('Latent Action Decoder Vector Field')
-    ani = FuncAnimation(fig, plot_function, interval=1)
-    plt.show()
-
 
 def simulate(
         decoder: cvae.VAE, 
@@ -162,10 +78,11 @@ if __name__ == '__main__':
             target=simulate, 
             args=(decoder, [conn_send_1, conn_send_2]))
     p_viz_vec = mp.Process(
-            target=visualize_vector_field, 
-            args=(decoder, conn_recv_1))
+            target=visualization.visualize_latent_actions_in_3d, 
+            args=(decoder, conn_recv_1, visualization.plot_vector_field))
     p_viz_man = mp.Process(
-            target=visualize_manifold, args=(decoder, conn_recv_2))
+            target=visualization.visualize_latent_actions_in_3d, 
+            args=(decoder, conn_recv_2, visualization.plot_manifold))
 
     p_sim.start()
     p_viz_vec.start()
---
+++ visualization.py
@@ -0,0 +1,93 @@
+import multiprocessing as mp
+import sys
+from typing import Callable
+
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
+import torch
+
+from cvae import cvae 
+
+ACTION_SCALE = 10
+
+def visualize_latent_actions_in_3d(
+        decoder: cvae.VAE, 
+        conn: mp.connection.Connection,
+        plot_function: Callable[
+            [int, cvae.VAE, mp.connection.Connection, matplotlib.axes.Axes, 
+                np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray], 
+            None]):
+    x, y, z = np.meshgrid(np.arange(-ACTION_SCALE, ACTION_SCALE, 1),
+                          np.arange(-ACTION_SCALE, ACTION_SCALE, 1),
+                          0)
+    latent_actions = np.concatenate((x, y), axis=-1)
+    latent_actions = torch.from_numpy(latent_actions.reshape((-1, 2))).float() 
+    latent_actions *= ACTION_SCALE
+    norm = np.linalg.norm(latent_actions, axis=-1)
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ani = FuncAnimation(
+            fig, plot_function, 
+            fargs=(decoder, conn, ax, latent_actions, x, y, z, norm),
+            interval=1)
+    plt.show()
+
+
+def plot_manifold(
+        frame: int, 
+        decoder: cvae.VAE, 
+        conn: mp.connection.Connection,
+        ax: matplotlib.axes.Axes, 
+        latent_actions: np.ndarray,
+        x: np.ndarray, y: np.ndarray, z: np.ndarray, c: np.ndarray):
+    ax.cla() 
+    
+    try:
+        prev_action, context = conn.recv()        
+    except:
+        sys.exit()
+
+    contexts = context.expand(latent_actions.shape[0], context.shape[1])
+    decoded_actions = _decode_latent_actions(
+            decoder, latent_actions, contexts, x)
+    u, v, w = np.split(decoded_actions, 3, axis=-1) 
+    sc = ax.scatter(u, v, w, c=c)
+
+
+def plot_vector_field(
+        frame: int, 
+        decoder: cvae.VAE, 
+        conn: mp.connection.Connection,
+        ax: matplotlib.axes.Axes, 
+        latent_actions: np.ndarray,
+        x: np.ndarray, y: np.ndarray, z: np.ndarray, c: np.ndarray):
+    ax.cla() 
+    
+    try:
+        prev_action, context = conn.recv()        
+    except Exception as e:
+        sys.exit()
+    
+    prev_action = prev_action.numpy()[0]
+    contexts = context.expand(latent_actions.shape[0], context.shape[1])
+    decoded_actions = _decode_latent_actions(
+            decoder, latent_actions, contexts, x)
+    u, v, w = np.split(decoded_actions, 3, axis=-1) 
+    ax.quiver(x, y, z, u, v, w, normalize=True)
+
+    # Controller/latent current posistion.
+    ax.plot(*prev_action, 'ro')
+    # Small hacks to 'equalize' axes since matplotlib doesn't support it.
+    ax.plot(0, 0, 2.5, alpha=0)
+    ax.plot(0, 0, -2.5, alpha=0)
+
+
+def _decode_latent_actions(decoder, latent_actions, contexts, x):
+    decoded_actions = decoder(latent=latent_actions, context=contexts)
+    decoded_actions = decoded_actions.detach().numpy()[:, :3]
+    decoded_actions = decoded_actions.reshape((x.shape[0], x.shape[1], 3))
+    return decoded_actions
