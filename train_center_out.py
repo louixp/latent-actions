66bdc156194a93a9dd3b407532dfdb5c8ee1f075
Louis Qin <qyl0509@icloud.com>
2022-07-04 14:14:06 -0700

    Restructured dataset modules.

--- train_center_out.py
+++ train_center_out.py
@@ -1,52 +1,15 @@
 from argparse import ArgumentParser
 import pickle
-from typing import Iterable, Tuple
 
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
 from pytorch_lightning import Trainer 
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.utilities.model_summary import ModelSummary
 
 from cvae.vae import VAE
-
-
-class CenterOutDemonstrationDataset(Dataset):
-
-    def __init__(self, 
-            path: str, *, 
-            radius_cutoff: float = np.inf,
-            size_limit: int = None):
-        
-        with open(path, "rb") as fp:
-            episodes = pickle.load(fp)
-
-        self.actions_joints = torch.tensor([
-                step['action_joints']
-                for episode in episodes for step in episode 
-                if step['radius'] < radius_cutoff], dtype=torch.float)
-        self.actions_ee = torch.tensor([
-                step['action_ee']
-                for episode in episodes for step in episode 
-                if step['radius'] < radius_cutoff], dtype=torch.float)
-        self.joint_angles = torch.tensor([
-                step['previous_joint_angles']
-                for episode in episodes for step in episode 
-                if step['radius'] < radius_cutoff], dtype=torch.float)
-
-        assert(len(self.actions_joints) == len(self.actions_ee) == len(self.joint_angles))
-
-        if size_limit is not None:
-            self.actions_joints = self.actions_joints[:size_limit]
-            self.actions_ee = self.actions_ee[:size_limit]
-        
-    def __len__(self) -> int:
-        return len(self.actions_joints)
-
-    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
-        return self.joint_angles[idx], self.actions_joints[idx] 
+from data.center_out import CenterOutDemonstrationDataset
 
 
 parser = ArgumentParser()
